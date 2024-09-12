20463801c9e3b1c89ebb600e9de33751b7a1a71c
PolyLvst <zeerofive05@gmail.com>
2024-09-12 09:01:25 +0700

    added capabilities to stay alive when bot crash, update config and filters

--- start_bot.bat
+++ start_bot.bat
@@ -11,8 +11,14 @@ if not exist "./venv" (
 )
 echo Sleeping 30s
 timeout /t 30 > nul
+:loop
 echo Checking adb devices
 adb connect localhost:5555
 adb devices
 python -m uiautomator2 init
 py run.py --config accounts/nama-akun-mu/config.yml
+$exitCode = $LASTEXITCODE
+if ($exitCode -gt 0) {
+    echo CRASH OCCURED, Staying alive ...
+    goto loop
+}
